c04f6e176d99a05265bfaecaa0454520c76d09c3
Taras Tarasiuk <taras_tarasiuk@epam.com>
2022-03-03 14:48:51 +0200

    added multilan

---
+++ js/language.js
@@ -0,0 +1,120 @@
+const arrLang = {
+    'pl': {
+        main_header: "O nas",
+        help_header: "Pomoc",
+        contacts_header: "Łączność",
+        about_help: "Pomóż Ukrainie",
+        about_who: "Jesteśmy ekipą wolontariuszy z Ukrainy, zorganizowaliśmy punkt zbioru niezbędnych rzeczy, które zostaną przekazane do wszystkich potrzebujących na Ukrainie.",
+        about_place: "Punkt zbioru: Gmina Horodło, ul. Łuszków, 4b Polska",
+        about_text: "Już teraz jesteśmy niezmiernie wdzięczni za wsparcie od wszystkich osób, które w chwili obecnej przebywają w Polsce oraz za to, że robicie wszystko, co jest możliwe, aby pomóc Ukrainie. Wiemy, że za granicą jest tak wielu ludzi, którzy są gotowi pomagać nam tak długo, jak tego potrzebujemy.",
+        about_do: "Aby wesprzeć Ukrainę, możesz wybrać jedną z poniższych opcji:",
+        help_section: "Pomoc",
+
+
+        aid_header: "Pomoc medyczna",
+        aid_text: "Lekarstwa są w tej chwili najważniejszą rzeczą. Jeśli się rozmyślasz, co jest lepiej – wnieść wkład pieniężny czy wysłać lekarstwa. Wybierz lekarstwa!",
+        aid_where: "Możesz kupić lekarstwa i wysłać je do naszego punktu zbioru pocztą (Gmina Horodło, ul. Łuszków, 4b, Polska).",
+        aid_what: "Lista niezbędnych leków w tej chwili, których katastroficznie brakuje na Ukrainie:",
+
+
+        food_header: "Żywność",
+        food_text: "Przed wysłaniem produktów zastanów się, jak długo można je przechowywać. Powiemy dlaczego: byliśmy świadkami, jak produkty, które zostały dostarczone na Ukrainę, trzeba było wyrzucić, ponieważ one się zepsuły w kilka dni. Pomóżmy, ale skutecznie!",
+        food_where: "Możesz kupić potrzebne produkty lub skorzystać z własnych zapasów żywności  i wysłać pocztą do naszego punktu zbioru (Gmina Horodło, ul. Łuszków, 4b, Polska).",
+        food_what: "Lista niezbędnych produktów w tej chwili:",
+
+        
+        clo_header: "Odzież",
+        clo_modal_header: "Odzież, okrycie, środki higieny i czystości",
+        clo_where: "Możesz wybrać coś z tej listy i wysłać pocztą do naszego punktu zbioru (Gmina Horodło, ul. Łuszków, 4b, Polska).",
+        clo_what: "Lista niezbędnych rzeczy w tej chwili:",
+
+        fin_header: "Zasoby finansowe",
+        fin_text: "Zobowiązujemy się do zgłaszania tutaj, na tej stronie internetowej, wszystkich kosztów, które zostaną poniesione w związku z Państwa wsparciem finansowym. W przypadku jakichkolwiek pytań, prosimy wysłać wiadomość na ten email: tarasiuk.sofi@gmail.com",
+        fin_bank_account: "Rachunek bankowy beneficjenta w banku korespondencyjnym",
+        fin_bank: "Bank korespondencyjny",
+
+
+        info_header: "Wsparcie informacyjne",
+        info_text: "Оpublikuj post w sieciach społecznościowych z linkiem kierującym do tej strony, aby Twoi przyjaciele, znajomi i wszyscy chętni do pomocy mogli zrobić to samo. Jedno kliknięcie – i już możesz nam pomóc w tej wojnie!",
+
+        
+        other_header: "Inny",
+        other_text: "Jeśli wiesz, jak możesz pomóc w inny sposób, też prosimy pisać na ten email: tarasiuk.sofi@gmail.com",
+
+        contacts_section: "Łączność",
+        footer_adr_header: "Adres magazynu",
+        footer_ard_text: "Gmina Horodło, ul. Łuszków, 4b, Polska",
+        footer_contacts: "Łączność"
+
+    },
+    'ua': {
+        main_header: "Про нас",
+        help_header: "Допомогти",
+        contacts_header: "Контакти",
+        about_help: "Допоможи Україні",
+        about_who: "Ми - команда волонтерів з України, які організували склад у Польщі для збору необхідних речей.",
+        about_place: "Місце складу: село Городло, вулиця Лужков, 4б",
+        about_text: "Ми вже надзвичайно вдячні за підтримку усіх, хто зараз знаходиться в Польщі і робить усе можливе, аби допомогти Україні. Знаємо, що закордоном є так багато тих, хто готовий нам допомагати стільки, скільки це буде потрібно.",
+        about_do: "Аби зробити свій внесок для допомоги України ви можете обрати будь-який із варіантів нижче:",
+        help_section: "Допомога",
+
+
+        aid_header: "МЕДИЧНА ДОПОМОГА",
+        aid_text: "Ліки - це найнеобхідніше з усього на цей момент. Якщо ви думаєте, що краще - зробити грошовий внесок, чи надіслати ліки. Обирайте ліки!",
+        aid_where: "Ви можете купити ліки та направити їх на наш склад за адресою село Городло, вулиця Лужков, 4б (Польща)",
+        aid_what: "Список необхідних ліків на цей момент:",
+
+
+
+        food_header: "їжа",
+        food_text: "Перш ніж відсилати продукти, подуймайте про те, наскільки довго вони можуть зберігатися. Бо ми були свідками того, як продукти, що надійшли в Україну доводилось викидати, бо вони за кілька днів зіпсувались. Давайте допомогати, але ефективно!",
+        food_where: "Ви можете купити потрібні продукти, або виділити їх зі своїх запасів та направити їх на наш склад за адресою село Городло, вулиця Лужков, 4б (Польща).",
+        food_what: "Список необхідних продуктів на цей момент:",
+
+
+        clo_header: "Одяг",
+        clo_modal_header: "Одяг та побутові предмети",
+        clo_where: "Ви можете обрати хоча б щось із цього списку та направити на наш склад за адресою село Городло, вулиця Лужков, 4б (Польща).",
+        clo_what: "Список необхідного на цей момент:",
+
+        
+        fin_header: "Фінансові ресурси",
+        fin_text: "Ми беремо на себе зобов’язання звітувати тут на цьому сайті про усі витрати, які будуть здійснені завдяки вашій фінансовій підтримці. З будь-яких питань - пишіть на пошту tarasiuk.sofi@gmail.com",
+        fin_bank_account: "Рахунок банку отримувача в банку-кореспонденті",
+        fin_bank: "Банк-кореспондент", 
+        
+
+
+        info_header: "Інформаційна підтримка",
+        info_text: "Зроби пост у соціальних мережах з лінком на цей сайт, аби ваші друзі, знайомі та усі охочі допомогти, могли це зробити. Один клік - і ви вже зможете допомогти нам боротися проти війни!",
+
+        other_header: "Інше",
+        other_text: "Можеш допомогти в інакший спосіб, то пиши сюди: tarasiuk.sofi@gmail.com",
+
+
+        
+        contacts_section: "Контакти",
+        footer_adr_header: "Адреса складу",
+        footer_ard_text: "село Городло, вулиця Лужков, 4б (Польща)",
+        footer_contacts: "Контакти"
+
+    }
+}
+
+$(function() {
+    $('.translate').click(function() {
+        let lang = $(this).attr('id');
+        $('.lang').each(function(index, item) {
+            $(this).text(arrLang[lang][$(this).attr('key')]);
+        })
+    })
+})
+
+
+// DEFAULT LANGUAGE
+window.onload = function() {
+    console.log("ff")
+    $('.lang').each(function(index, item) {
+        $(this).text(arrLang['ua'][$(this).attr('key')]);
+    })
+};
